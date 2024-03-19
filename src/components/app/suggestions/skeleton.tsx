c76c3f51ce76e54f128770ba88e60cefaf96808a
Victor1890 <victorrosariodeveloper@gmail.com>
2024-03-19 11:33:04 -0400

    refactor: :recycle: add skeleton suggestion items

--- src/components/app/suggestions/skeleton.tsx
+++ src/components/app/suggestions/skeleton.tsx
@@ -2,6 +2,16 @@ import { Skeleton } from "@nextui-org/react";
 
 function SuggestionSkeleton() {
     return (
+        <div className="grid grid-cols-2 gap-5">
+            <SuggestionSkeletonItem />
+            <SuggestionSkeletonItem />
+            <SuggestionSkeletonItem />
+        </div>
+    );
+}
+
+function SuggestionSkeletonItem() {
+    return (
         <article className="border-1 border-solid rounded-lg overflow-hidden">
             <Skeleton className="aspect-square" />
             <div className="border-t border-solid h-10" />
