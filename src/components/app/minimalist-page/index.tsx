965885e3b6fa0a31f6d09487047107db1fb06901
Victor1890 <victorrosariodeveloper@gmail.com>
2024-03-20 13:43:35 -0400

    refactor: :recycle: refactor MinimalistPage component

--- src/components/app/minimalist-page/index.tsx
+++ src/components/app/minimalist-page/index.tsx
@@ -1,15 +1,24 @@
-import { useColorStore } from "@/stores/colors";
-import { useState } from "react";
+'use client'
+
+import { applyStyle } from "@/utils/color.util";
+import {
+    WindowMockup,
+    Navbar,
+    Hero,
+    About
+} from "./components";
+
+
 
 function MinimalistPage() {
-
-    const [bg, setBg] = useState<string>("");
-
-    useColorStore.subscribe(({ bg }) => setBg(bg));
-
     return (
-        <div style={{ backgroundColor: bg }} className="shadow-xl rounded-xl border-1 border-solid border-gray-200 overflow-hidden ">
-
+        <div style={applyStyle('bg')} className="shadow-xl rounded-xl border-1 border-solid border-gray-200 overflow-hidden w-full">
+            <WindowMockup />
+            <Navbar />
+            <div className="flex flex-col gap-12 mt-8 lg:gap-16">
+                <Hero />
+                <About />
+            </div>
         </div>
     )
 }
