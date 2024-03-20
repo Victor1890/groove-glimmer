6797ae0080f9d6d75e40c61b0d7f0f95d9aec6ce
Victor1890 <victorrosariodeveloper@gmail.com>
2024-03-20 13:46:31 -0400

    feat: :triangular_flag_on_post: add color variables and update styles in preview page

--- src/components/pages/preview/index.tsx
+++ src/components/pages/preview/index.tsx
@@ -3,6 +3,8 @@
 import MinimalistPage from "@/components/app/minimalist-page";
 import { ToggleButton, ToggleButtonItem } from "@/components/app/toggle-button"
 import { useColorStore } from "@/stores/colors"
+import { bgVar, fgVar } from "@/utils/color.util";
+import Color from "color";
 import { useState } from "react";
 
 const previewModes = {
@@ -13,10 +15,21 @@ const previewModes = {
 const Preview = () => {
 
     const [previewMode, setPreviewMode] = useState<string>(previewModes.minimalistPage);
-    const { bg, fg } = useColorStore((state) => state)
+    const [{ bgRgb, fgRgb }, setColors] = useState<{ bgRgb: string, fgRgb: string }>({ bgRgb: "", fgRgb: "" });
+
+    useColorStore.subscribe(({ bg, fg }) => {
+        const bgRgb = Color(bg).rgb().round().array().join(" ");
+        const fgRgb = Color(fg).rgb().round().array().join(" ");
+
+        setColors({ bgRgb, fgRgb })
+    })
 
     return (
-        <div className="max-w-xl mx-auto px-4 md:px-6">
+        <div style={{
+            marginInline: "auto",
+            [fgVar as string]: fgRgb,
+            [bgVar as string]: bgRgb
+        }} className="w-full px-4 md:px-6">
             <section className="my-6">
                 <div className="text-center mb-6">
                     <ToggleButton defaultValue={previewMode} onChange={setPreviewMode}>
