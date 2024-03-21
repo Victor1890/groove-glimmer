72c3cae0cd796b2dba0439c8cb48bf5b78b84fc1
Victor1890 <victorrosariodeveloper@gmail.com>
2024-03-21 13:55:00 -0400

    refactor: :recycle: refactor sidebar and layout components

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -9,16 +9,20 @@ interface Props {
 
 export const Layout = ({ children }: Props) => {
   return (
-    <main className="relative flex flex-col flex-1">
+    <main className="flex flex-col">
       <Navbar />
-      <div className="flex flex-col lg:flex-row">
+      <div className="flex flex-col lg:flex-row overflow-auto">
         <Sidebar />
-        <section className="bg-tertiary flex flex-col w-full overflow-auto">
+        <section className="bg-tertiary flex flex-col flex-1 w-full overflow-auto h-auto md:h-screen">
+          {children}
+          <Footer />
+        </section>
+        {/* <section className="bg-tertiary flex flex-col">
           <div className="h-screen">
             {children}
             <Footer />
           </div>
-        </section>
+        </section> */}
       </div>
     </main>
   );
